f1fec68d237a0f7c90f993e5a46943295a8a5a01
pawel <pawelsk.vp.pl@gmial.com>
2021-03-28 00:10:50 +0100

    less code in main, same functionality (i hope)

--- main.cpp
+++ main.cpp
@@ -15,13 +15,15 @@ int main() // all comunicates shown to the user are written in Polish since this
         std::cout<<help_info;
         std::string inp_msg;
         std::cin>>inp_msg;
-        if(inp_msg == "sprzedaj")
+        if(inp_msg == "sprzedaj" || inp_msg == "kup")
         {
             std::string currency;
             int am_big_ones, am_small_ones;
-            std::cout<<av_curr<<"Jaką walutę chcesz sprzedać?\n";
+            if(inp_msg == "sprzedaj")std::cout<<av_curr<<"Jaką walutę chcesz sprzedać?\n";
+            else std::cout<<av_curr<<"Jaką walutę chcesz kupić?\n";
             std::cin>>currency;
-            std::cout<<"Ile odpowiedników złotówki chcesz sprzedać?\n";
+            if(inp_msg == "sprzedaj")std::cout<<"Ile odpowiedników złotówki chcesz sprzedać?\n";
+            else std::cout<<"Ile odpowiedników złotówki chcesz kupić?\n";
             std::cin>>am_big_ones;
             if(std::cin.fail() || am_big_ones<0)
             {
@@ -30,7 +32,8 @@ int main() // all comunicates shown to the user are written in Polish since this
                 std::cin.ignore( std::numeric_limits < std::streamsize >::max(), '\n' );
                 continue;
             }
-            std::cout<<"Ile odpowiedników groszy chcesz sprzedać?\n";
+            if(inp_msg == "sprzedaj")std::cout<<"Ile odpowiedników groszy chcesz sprzedać?\n";
+            else std::cout<<"Ile odpowiedników groszy chcesz kupić?\n";
             std::cin>>am_small_ones;
             if(std::cin.fail() || am_small_ones<0)
             {
@@ -46,41 +49,8 @@ int main() // all comunicates shown to the user are written in Polish since this
                 std::cout<<"Podana waluta nie jest sprzedawana w tym kantorze\n"<<av_curr;
                 continue;
             }
-            std::cout<<"Sprzedano ";
-            calculate_and_print(amount, the_rate, currency);
-        }
-        else if(inp_msg == "kup")
-        {
-            std::string currency;
-            int am_big_ones, am_small_ones;
-            std::cout<<av_curr<<"Jaką walutę chcesz kupić?\n";
-            std::cin>>currency;
-            std::cout<<"Ile odpowiedników złotówki chcesz kupić?\n";
-            std::cin>>am_big_ones;
-            if(std::cin.fail() || am_big_ones<0)
-            {
-                std::cout<<"Podano niewłaściwą ilość odpwiedników złotówek\n";
-                std::cin.clear();
-                std::cin.ignore( std::numeric_limits < std::streamsize >::max(), '\n' );
-                continue;
-            }
-            std::cout<<"Ile odpowiedników groszy chcesz kupić?\n";
-            std::cin>>am_small_ones;
-            if(std::cin.fail() || am_small_ones<0)
-            {
-                std::cout<<"Podano niewłaściwą ilość odpwiedników groszy\n";
-                std::cin.clear();
-                std::cin.ignore( std::numeric_limits < std::streamsize >::max(), '\n' );
-                continue;
-            }
-            int amount = am_big_ones * 100 + am_small_ones;
-            int the_rate = rate(currency, false);
-            if(the_rate == -1) // -1 was chosen as a error code to comunicate that the entered currency name is not in the array of availabe currencies
-            {
-                std::cout<<"Podana waluta nie jest sprzedawana w tym kantorze\n"<<av_curr;
-                continue;
-            }
-            std::cout<<"Kupiono ";
+            if(inp_msg == "sprzedaj")std::cout<<"Sprzedano ";
+            else std::cout<<"Kupiono ";
             calculate_and_print(amount, the_rate, currency);
         }
         else if(inp_msg == "exit")
